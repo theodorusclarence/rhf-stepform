c35c101e052c0d895d5ecf3b79567a3bfef52e0e
rizqitsani <rizqitsani@gmail.com>
2021-08-28 11:14:27 +0700

    fix: can't delete on revisit

--- components/Forms/DropzoneInput.tsx
+++ components/Forms/DropzoneInput.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
-import { MouseEvent, useCallback } from 'react';
-import { useFormContext } from 'react-hook-form';
-import { FileWithPath, useDropzone } from 'react-dropzone';
+import { MouseEvent, useCallback, useEffect, useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { useDropzone } from 'react-dropzone';
 
 import { FilePreview } from './FilePreview';
 
@@ -25,7 +25,8 @@ export default function DropzoneInput({
   readOnly,
 }: DropzoneInputProps) {
   const {
-    register,
+    control,
+    getValues,
     setValue,
     setError,
     clearErrors,
@@ -33,7 +34,8 @@ export default function DropzoneInput({
     formState: { errors },
   } = useFormContext();
 
-  const files: FileWithPath[] = watch(id);
+  const [files, setFiles] = useState<any[]>(getValues(id) || []);
+
   const onDrop = useCallback(
     (acceptedFiles, rejectedFiles) => {
       if (rejectedFiles && rejectedFiles.length > 0) {
@@ -43,6 +45,18 @@ export default function DropzoneInput({
           message: rejectedFiles && rejectedFiles[0].errors[0].message,
         });
       } else {
+        const acceptedFilesPreview = acceptedFiles.map((file: any) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        );
+
+        setFiles(
+          files
+            ? [...files, ...acceptedFilesPreview].slice(0, maxFiles)
+            : acceptedFilesPreview
+        );
+
         setValue(
           id,
           files
@@ -58,15 +72,25 @@ export default function DropzoneInput({
     [clearErrors, files, id, maxFiles, setError, setValue]
   );
 
-  const deleteFile = (e: MouseEvent, file: FileWithPath) => {
+  useEffect(() => {
+    return () => {
+      () => {
+        files.forEach((file) => URL.revokeObjectURL(file.preview));
+      };
+    };
+  }, [files]);
+
+  const deleteFile = (e: MouseEvent, file: any) => {
     e.preventDefault();
     const newFiles = [...files];
 
     newFiles.splice(newFiles.indexOf(file), 1);
 
     if (newFiles.length > 0) {
+      setFiles(newFiles);
       setValue(id, newFiles);
     } else {
+      setFiles([]);
       setValue(id, []);
     }
   };
@@ -100,53 +124,61 @@ export default function DropzoneInput({
           ))}
         </ul>
       ) : (
-        <>
-          <div
-            className='mt-1 focus:outline-none focus:ring-dark-400 group'
-            {...getRootProps()}
-          >
-            <input {...register(id, validation)} id={id} {...getInputProps()} />
-            <div
-              className={clsx(
-                'w-full p-2 bg-gray-100 border border-gray-300 border-dashed rounded cursor-pointer',
-                errors[id]
-                  ? 'border-red-500 group-focus:border-red-500'
-                  : 'group-focus:border-primary-500'
-              )}
-            >
-              <div className='my-20 space-y-2 text-center'>
-                <p className='text-gray-500'>
-                  Drag &apos;n&apos; drop some files here, or click to select
-                  files
-                </p>
-                <p className='text-xs text-gray-500'>{`${
-                  maxFiles - (files?.length || 0)
-                } file(s) remaining`}</p>
+        <Controller
+          control={control}
+          name={id}
+          rules={validation}
+          render={(controllerProps) => (
+            <>
+              <div
+                className='mt-1 focus:outline-none focus:ring-dark-400 group'
+                {...getRootProps()}
+                {...controllerProps}
+              >
+                <input {...getInputProps()} />
+                <div
+                  className={clsx(
+                    'w-full p-2 bg-gray-100 border border-gray-300 border-dashed rounded cursor-pointer',
+                    errors[id]
+                      ? 'border-red-500 group-focus:border-red-500'
+                      : 'group-focus:border-primary-500'
+                  )}
+                >
+                  <div className='my-20 space-y-2 text-center'>
+                    <p className='text-gray-500'>
+                      Drag &apos;n&apos; drop some files here, or click to
+                      select files
+                    </p>
+                    <p className='text-xs text-gray-500'>{`${
+                      maxFiles - (files?.length || 0)
+                    } file(s) remaining`}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div className='mt-1'>
-            {helperText !== '' && (
-              <p className='text-xs text-gray-500'>{helperText}</p>
-            )}
-            {errors[id] && (
-              <p className='text-sm text-red-500'>{errors[id].message}</p>
-            )}
-          </div>
-          {!readOnly && !!files?.length && (
-            <ul className='border border-gray-300 divide-y divide-gray-300 rounded-md'>
-              {files.map((file, index) => (
-                <FilePreview
-                  key={index}
-                  readOnly={readOnly}
-                  file={file}
-                  deleteFile={deleteFile}
-                />
-              ))}
-            </ul>
+              <div className='mt-1'>
+                {helperText !== '' && (
+                  <p className='text-xs text-gray-500'>{helperText}</p>
+                )}
+                {errors[id] && (
+                  <p className='text-sm text-red-500'>{errors[id].message}</p>
+                )}
+              </div>
+              {!readOnly && !!files?.length && (
+                <ul className='border border-gray-300 divide-y divide-gray-300 rounded-md'>
+                  {files.map((file, index) => (
+                    <FilePreview
+                      key={index}
+                      readOnly={readOnly}
+                      file={file}
+                      deleteFile={deleteFile}
+                    />
+                  ))}
+                </ul>
+              )}
+            </>
           )}
-        </>
+        />
       )}
     </>
   );
